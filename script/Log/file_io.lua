e5a0ad005fd27cccc1152549f8d4c722a7e64f41
zhm <koumei.rin@qq.com>
2018-08-27 20:24:39 +0800

    新增日志模块，先存档，在别的项目开发中

---
+++ script/Log/file_io.lua
@@ -0,0 +1,36 @@
+require "anxin1225_lua_behaviour_tree.Log.base_class"
+
+FileIO = FileIO or BaseClass()
+
+function FileIO:__init()
+    -- 默认日志访问路径
+    self.file_path = "./log/"
+end
+
+
+-- 在访问文件的时候，会自行打开文件
+function FileIO:GetFile(file_name)
+    if not self.file[file_name] then
+        self.file[file_name] = io.open(string.format("%s.%s", file_name, "log"), "a+")
+    end
+    if io.type(self.file[file_name]) ~= "file" then 
+        self.file[file_name] = io.open(string.format("%s.%s", file_name, "log"), "a+")
+    end
+    return self.file[file_name]
+end
+
+function FileIO:Write(file_name, obj)
+    -- self.file[file_name]:Write(tostring(obj))
+    self.file[file_name] = 1
+    
+end
+
+
+
+-- 在访问文件的时候，会自行打开文件
+-- 在清除文件引用的时候，会自行关闭文件通道
+
+
+-- test
+FileIO.Write("fileio", "msg")
+FileIO.Write("fileio", "msg")
