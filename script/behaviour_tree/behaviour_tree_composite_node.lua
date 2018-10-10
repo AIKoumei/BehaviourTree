c29f24f4b1111d4e40e4490a80c3d8e488eee775
zhm <koumei.rin@qq.com>
2018-10-10 21:55:43 +0800

    漏提交，补一下

--- script/behaviour_tree/behaviour_tree_composite_node.lua
+++ script/behaviour_tree/behaviour_tree_composite_node.lua
@@ -5,7 +5,3 @@ BehaviourTreeCompositeNode = BehaviourTreeCompositeNode or BaseClass(BehaviourTr
 ------------------------------------------------
 -- # 方法
 ------------------------------------------------
--- 接受运行结果
--- 当节点运行完毕，返回给父节点的时候，调用该方法，更新下次运行的节点
-function BehaviourTreeCompositeNode:ReciveRunResult(entity, running_node, result)
-end
